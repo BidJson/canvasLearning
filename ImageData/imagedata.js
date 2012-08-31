88c8dfbc1f51e4bdb69d5b4cd7de7129f6e8a19c
xiaohuai <xh5683kj@hotmail.com>
2012-08-31 19:12:25 +0800

    rotateX & rotateY & movingZ

--- ImageData/imagedata.js
+++ ImageData/imagedata.js
@@ -9,9 +9,10 @@
 		this.imgWidth = 0;
 		this.imgHeight = 0;
 		this.isRotate = false;
-		this.angleSpeed = 1;
+		this.angleSpeed_x = this.angleSpeed_y = 1;
+		this.step_z = 1;
+		this.speed_dir = 1;
 		this.range = function(value, a, b){
-			return value;
 			var max = Math.max(a, b), min = Math.min(a, b);
 			if(value < min || value > max){
 				return Math.random() * (max - min) + min;
@@ -31,7 +32,7 @@
 			ctx = this.ctx;
 			_imgData = null,
 			_data = null,
-			_pixels = [],
+			_pixels = [];
 			
 		ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 		ctx.drawImage(this.img, 0, 0, iw, ih, cw / 2 - iw / 4, ch / 2 - ih / 4, iw / 2, ih / 2);
@@ -44,28 +45,26 @@
 		
 		// 生成像素
 		for(var i = 0; i < _imgData.height; i++){
-			var _r = [];
 			for(var j = 0; j < _imgData.width; j++){
 				var _index = 4 * (i * _imgData.width + j);
 				if(_data[_index+3] > 128){  //半透明以上的像素点
 					var x = (j - _imgData.width / 2),
 						y = (i - _imgData.height / 2),
 						_color = {
-							r : this.range(_data[_index], 210, 256),
-							g : this.range(_data[_index+1], 210, 256),
-							b : this.range(_data[_index+2], 210, 256)
+							r : _data[_index],
+							g : _data[_index+1],
+							b : _data[_index+2]
 						},
-					    _p = ImgData.createParticle(cw / 2, ch / 2, 0, x, y, 0, 2.5, ctx, _color, _data[_index+3]);
+					    _p = ImgData.createParticle(cw / 2, ch / 2, 0, x, y, 0, 2.5, ctx, _color, _data[_index+3], this);
 					    
-					_r.push(_p);
+					_pixels.push(_p);
 				}
 			}
-			_pixels.push(_r);
 		}
-		_pixels[0][18]._log = true;
+		_pixels[0]._log = true;
 		ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-		
 		this.pixels = _pixels;
+		console.debug(_pixels);
 	}
 	
 	ptp.draw = function(){
@@ -75,18 +74,12 @@
 		ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 		
 		// 绘制像素
-		for(var i = 0; i < _pixels.length; i++){
-			for(var j = 0; j < _pixels[i].length; j++){
-				_pixels[i][j].render();
-			}
-		}
+		for(var i = 0; i < _pixels.length; i++) _pixels[i].render();
 	}
 	
 	ptp.rotateY = function(angle){
 		for(var i = 0; i < this.pixels.length; i++){
-			for(var j = 0; j < this.pixels[i].length; j++){
-				this.pixels[i][j].rotateY(angle);
-			}
+			this.pixels[i].rotateY(angle);
 		}
 	}
 	
@@ -95,7 +88,8 @@
 	}
 	
 	ptp.setCenterPoint = function(x, y ,z){
-		
+		for(var i = 0; i < this.pixels.length; i++) 
+			for(var j = 0; j < this.pixels[i]; j++) this.pixels[i][j].setCenterPoint(x, y, z);
 	};
 	
 	ptp.setVanishPoint = function(x, y, z){
@@ -107,25 +101,39 @@
 		this.draw();
 	}
 	
+	ptp.update = function(){
+		for(var i = 0; i < this.pixels.length; i++){
+			var _p = this.pixels[i];
+			var _pos = _p.getScreenXY();
+			if(this.isRotate){
+				_p.rotateY(Math.PI/180 * this.angleSpeed_y);
+				_p.rotateX(Math.PI/180 * this.angleSpeed_x);
+			}
+			_p.zpos += this.step_z * 0.5;
+			_p.size = 2.5 * Math.max(_p.getScale(), 0.5);
+			_p.x = _pos.x;
+			_p.y = _pos.y;
+		}
+	}
+	
 	ptp = null;
 	
-	ImgData.createParticle = function(cx, cy, cz, x, y, z, size, ctx, color, alpha){
+	ImgData.createParticle = function(cx, cy, cz, x, y, z, size, ctx, color, alpha, parent){
 		var p = CVS.createPoint3D(ctx, function(){
-			this.setCenterPoint(cx, cy, cz);
+			this.setCenterPoint(0, 0, 0);
 			this.setVanishPoint(cx, cy);
-			this.xpos = x;
-			this.ypos = y;
+			this.xpos = x*6;
+			this.ypos = y*6;
 			this.zpos = z;
 			this.size = size;
 			this.color = new CVS.$color(color.r, color.g, color.b);
 			this.alpha = alpha;
 			this.draw = function(){
-				var _scale = ImgData.valueBetween(this.getScale(), 0.5, 2);
-				_scale = this.getScale();
-				if(this._log) console.log(this.alpha * _scale);
-				ctx.fillStyle = 'rgba(' + this.color.r + ',' + this.color.g +',' + this.color.b + ',' + this.alpha * _scale + ')';
+				//var _scale = ImgData.valueBetween(this.getScale(), 0.5, 2);
+				var _scale = this.getScale();
+				ctx.fillStyle = 'rgba(' + this.color.r + ',' + this.color.g +',' + this.color.b + ',' + /*this.alpha * ImgData.valueBetween(_scale, 1, 0.3) /*/ 256 + ')';
 				ctx.beginPath();
-				ctx.arc(cx+this.xpos*6*_scale, cy+this.ypos*6*_scale, this.size * _scale, 0, Math.PI * 2, true);
+				ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, true);
 				ctx.closePath();
 				ctx.fill();
 			}
